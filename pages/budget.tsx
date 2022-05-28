50974a29976e2494c1bb89a66998b0f7f2761b25
Oliver White <whiteoliverg@gmail.com>
2022-05-28 14:04:23 -0400

    feat: basic budget with errors

--- pages/budget.tsx
+++ pages/budget.tsx
@@ -1,37 +1,46 @@
 import Day from '../components/budget/Day';
+import useLocalStorage from 'react-localstorage-hook';
+import AddLineItem from '../components/budget/AddLineItem';
+export type budgetItem = {
+    title: string;
+    date: string;
+    direction: string;
+    value: number;
+    currency: string;
+};
 
+const itemsToDays = (items: budgetItem[]): { [k: string]: budgetItem[] } => {
+    const itemsInDays = items.reduce((previousValue: { [k: string]: budgetItem[] }, currentValue) => {
+        const dateString = currentValue.date;
+        if (previousValue[dateString]) {
+            previousValue[dateString].push(currentValue);
+        } else {
+            previousValue[dateString] = [currentValue];
+        }
+        return previousValue;
+    }, {});
+    const ordered = Object.keys(itemsInDays)
+        .sort()
+        .reverse()
+        .reduce((obj: { [k: string]: budgetItem[] }, key) => {
+            obj[key] = itemsInDays[key];
+            return obj;
+        }, {});
+    return ordered;
+};
 const Budget = () => {
+    let itemsAsDays: { [k: string]: budgetItem[] } = {};
+    if (typeof window !== 'undefined') {
+        const [items]: [budgetItem[]] = useLocalStorage('items', []);
+        itemsAsDays = itemsToDays(items);
+    }
+
     return (
         <div>
-            <Day
-                date="31 May"
-                lineItems={[
-                    {
-                        title: 'Food',
-                        direction: 'out',
-                        value: 44.54,
-                        currency: 'CAD',
-                    },
-                ]}
-            />
-
-            <Day
-                date="30 May"
-                lineItems={[
-                    {
-                        title: 'Video Game',
-                        direction: 'out',
-                        value: 1100,
-                        currency: 'HUF',
-                    },
-                    {
-                        title: 'Discord Nitro',
-                        direction: 'out',
-                        value: 5,
-                        currency: 'EUR',
-                    },
-                ]}
-            />
+            <AddLineItem />
+            {Object.values(itemsAsDays).map((value) => (
+                <Day date={value[0].date} lineItems={value} />
+            ))}
         </div>
     );
 };
