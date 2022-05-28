c24e768c8b38d6e99fb9a09ed2011a621d26f32e
Oliver White <whiteoliverg@gmail.com>
2022-05-28 19:33:42 -0400

    feat: add layout and menu bar

---
+++ components/navigation/Menu.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { List } from 'react-bootstrap-icons';
+import { useRouter } from 'next/router';
+
+const Menu = () => {
+    const [show, setShow] = useState(false);
+
+    const router = useRouter();
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Nav fill variant="tabs" activeKey={router.asPath}>
+                <Nav.Item>
+                    <Nav.Link onClick={handleShow}>
+                        <List />
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="/">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="/budget">Budget History</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <Offcanvas show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Settings</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>Here is where the settings will be located</Offcanvas.Body>
+            </Offcanvas>
+        </>
+    );
+};
+
+export default Menu;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,30 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.css';
+import Menu from '../components/navigation/Menu';
+import styles from '../styles/Home.module.css';
+import Image from 'next/image';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Menu />
+            <Component {...pageProps} />
+
+            <footer className={styles.footer}>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{' '}
+                    <span className={styles.logo}>
+                        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                    </span>
+                </a>
+            </footer>
+        </>
+    );
 }
 
 export default MyApp;
