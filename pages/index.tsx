ed23441be35f6b6633300fd2f0777e4e98a5bd9c
Oliver White <whiteoliverg@gmail.com>
2022-05-29 13:36:55 -0400

    feat: add month pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,33 @@ import useStorage from '../hooks/useStorage';
 import { useState } from 'react';
 import dynamic from 'next/dynamic';
 import { budgetItem } from '../types';
+import { addMonths, endOfMonth, format, isAfter, isBefore, parse, startOfMonth, subMonths } from 'date-fns';
+import { Button } from 'react-bootstrap';
+import { CaretLeft, CaretRight } from 'react-bootstrap-icons';
+import { styled } from '@stitches/react';
 
-const itemsToDays = (items: budgetItem[]): { [k: string]: budgetItem[] } => {
-    const itemsInDays = items.reduce((previousValue: { [k: string]: budgetItem[] }, currentValue) => {
-        const dateString = currentValue.date;
-        if (previousValue[dateString]) {
-            previousValue[dateString].push(currentValue);
-        } else {
-            previousValue[dateString] = [currentValue];
-        }
-        return previousValue;
-    }, {});
+export const TitleContainer = styled('div', {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
+    marginRight: '15px',
+});
+
+const itemsToDays = (items: budgetItem[], start: Date, end: Date): { [k: string]: budgetItem[] } => {
+    const itemsInDays = items
+        .filter((item) => {
+            const itemDate = parse(item.date, 'yyyy-MM-dd', new Date());
+            return isBefore(itemDate, end) && isAfter(itemDate, start);
+        })
+        .reduce((previousValue: { [k: string]: budgetItem[] }, currentValue) => {
+            const dateString = currentValue.date;
+            if (previousValue[dateString]) {
+                previousValue[dateString].push(currentValue);
+            } else {
+                previousValue[dateString] = [currentValue];
+            }
+            return previousValue;
+        }, {});
     const ordered = Object.keys(itemsInDays)
         .sort()
         .reverse()
@@ -29,15 +45,27 @@ const Budget = () => {
     const { getItem } = useStorage();
     const localItems = getItem('items', [], 'local');
     const [items, setItems] = useState(localItems);
-    const itemsAsDays = itemsToDays(items);
 
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const itemsAsDays = itemsToDays(items, startOfMonth(currentDate), endOfMonth(currentDate));
     return (
-        <div>
-            <AddLineItem addItem={(item) => setItems([...items, item])} />
-            {Object.values(itemsAsDays).map((value) => (
-                <Day date={value[0].date} lineItems={value} key={value[0].date} />
-            ))}
-        </div>
+        <>
+            <TitleContainer>
+                <Button variant="outline-info" onClick={() => setCurrentDate(subMonths(currentDate, 1))}>
+                    <CaretLeft />
+                </Button>
+                <h1>{format(currentDate, 'MMMM')}</h1>
+                <Button variant="outline-info" onClick={() => setCurrentDate(addMonths(currentDate, 1))}>
+                    <CaretRight />
+                </Button>
+            </TitleContainer>
+            <div>
+                <AddLineItem addItem={(item) => setItems([...items, item])} />
+                {Object.values(itemsAsDays).map((value) => (
+                    <Day date={value[0].date} lineItems={value} key={value[0].date} />
+                ))}
+            </div>
+        </>
     );
 };
 
